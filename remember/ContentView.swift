ae17ad85579bc170780ca4fea55c725e067197f4
Ing. Ebu Celik <ebucelik1@hotmail.com>
2025-09-22 09:18:00 +0200

    Initial Commit

---
+++ remember/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  remember
+//
+//  Created by Ing. Ebu Bekir Celik, BSc, MSc on 22.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ remember/rememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  rememberApp.swift
+//  remember
+//
+//  Created by Ing. Ebu Bekir Celik, BSc, MSc on 22.09.25.
+//
+
+import SwiftUI
+
+@main
+struct rememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
